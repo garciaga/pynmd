8d47734117ad3c7eb6e142b1e5810fb6b452a966
Gabriel Garcia Medina <gabrielgarciamedina@gmail.com>
2016-04-11 12:51:28 -0700

    improved performance of depth averaging for nhwave

--- models/nhwave/post/nhwave_post.py
+++ models/nhwave/post/nhwave_post.py
@@ -826,23 +826,32 @@ def depth_average(u,eta,h):
         
     """
     
-    # Preallocate variables
-    ubar = np.zeros_like(eta) * np.NAN
-       
-    # Compute vertical coordinate system
-    ds    = 1.0/u.shape[0]
-    #sigma = np.arange(ds/2.0,1-ds/2.0,ds)
+#     Old CODE
+#     # Preallocate variables
+#     ubar = np.zeros_like(eta) * np.NAN
+#        
+#     # Compute vertical coordinate system
+#     ds    = 1.0/u.shape[0]
+#     #sigma = np.arange(ds/2.0,1-ds/2.0,ds)
+#     
+#     # Loop over the array
+#     for aa in range(ubar.shape[0]):
+#         
+#         # Total water depth
+#         D = eta[aa] + h[aa]
+#         
+#         # Depth average
+#         ubar[aa] =  np.sum(ds*D*u[:,aa])/D
+#     
+#     return ubar
+
+    # Compute the vertical coordinates
+    ds = 1.0/u.shape[0]
     
-    # Loop over the array
-    for aa in range(ubar.shape[0]):
-        
-        # Total water depth
-        D = eta[aa] + h[aa]
-        
-        # Depth average
-        ubar[aa] =  np.sum(ds*D*u[:,aa])/D
+    # Compute total water depth
+    D = eta + h
     
-    return ubar
+    return np.sum(ds*D*u,axis=0)/D
     
     
 #===============================================================================
