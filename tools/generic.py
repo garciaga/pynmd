1b07d9cade75625851d1286666457f4ba984b825
Gabriel Garcia Medina <gabrielgarciamedina@gmail.com>
2016-11-10 10:28:24 -0800

    Added generic python tools

---
+++ tools/generic.py
@@ -0,0 +1,17 @@
+"""
+Generic python tools.
+"""
+
+from __future__ import division,print_function
+
+def flatten_list_of_lists(x):
+    """
+    Code to flatten a list of lists
+    
+    Reference:
+    ----------
+    http://stackoverflow.com/questions/952914/making-a-flat-list-out-of-list-of-lists-in-python
+    """
+    
+    return [item for sublist in x for item in sublist]
+
