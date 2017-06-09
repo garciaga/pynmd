f8b3af0b3013bd7dfb61b399f19df3d8cd5acb98
Gabriel Garcia Medina <gabrielgarciamedina@gmail.com>
2017-06-09 10:46:11 -0700

    fixed runup xbeach 2D

--- models/xbeach/post/xbeach_post.py
+++ models/xbeach/post/xbeach_post.py
@@ -14,7 +14,7 @@ from __future__ import division,print_function
 import numpy as np
 
 # Compute runup
-def nc_runup(nc,r_depth=0.01):
+def nc_runup(nc,r_depth=0.01,yind=None):
     """
     
     Function to compute runup from netcdf file.
@@ -32,35 +32,46 @@ def nc_runup(nc,r_depth=0.01):
                        
     Notes:
     ------
-    
+    1. Test for 1D simulations.
     """
     
-    # Load variables
-    h   = nc.variables['zb'][:]
-    eta = nc.variables['zs'][:]
-    d   = eta - h
-    x   = nc.variables['globalx'][:]    
+    # See if this is a 1d or 3d simulation
+    simShape = nc.variables['zb'].shape
+    # 2DH simulation
+    if len(simShape) == 3:
+        
+        if yind is None:
+            yind = np.round(simShape[1]/2)
+            
+        h   = nc.variables['zb'][:,yind,:].squeeze()
+        eta = nc.variables['zs'][:,yind,:].squeeze()
+        d   = eta - h
+        x   = nc.variables['globalx'][yind,:].squeeze()    
     
-    # Need to manage 2d files
-    
+    # 1D simulation   
+    else:        
+        
+        # Load variables
+        h   = nc.variables['zb'][:]
+        eta = nc.variables['zs'][:]
+        d   = eta - h
+        x   = nc.variables['globalx'][:]    
+       
     # Preallocate runup variable
-    z_runup = np.zeros((d.shape[0],d.shape[1]))
+    z_runup = np.zeros((d.shape[0],))
     x_runup = np.zeros_like(z_runup)
     
     # Loop over time, which is assumed to be on the first dimension.
     for aa in range(z_runup.shape[0]):
+                   
+        # Find the runup contour (search from left to right) 
+        wdepth_ind = np.argmin(d[aa,:]>r_depth)-1
         
-        # Loop over space
-        for bb in range(z_runup.shape[1]):
-            
-            # Find the runup contour (search from left to right) 
-            wdepth_ind = np.argmin(d[aa,bb,:]>r_depth)-1
-            
-            # Store the water surface elevation in matrix
-            z_runup[aa,bb]= eta[aa,bb,wdepth_ind]
-            
-            # Store runup position
-            x_runup[aa,bb] = x[bb,wdepth_ind]
+        # Store the water surface elevation in matrix
+        z_runup[aa]= h[aa,wdepth_ind]
+        
+        # Store runup position
+        x_runup[aa] = x[wdepth_ind]
     
     # Done
     return z_runup,x_runup
