0a78240144e0bf876ba40a03e8e237be2517b362
saeed moghimi account <saeed.moghimi.lx@ocs-s-lnxapp01.nos.noaa>
2017-07-27 13:37:52 -0400

    add stick plot

--- plotting/plot_routines.py
+++ plotting/plot_routines.py
@@ -707,5 +707,31 @@ def ncks(param='zeta',xvar='eta_rho',yvar='xi_rho',ix=0,jy=0,filein='tmp.nc',fil
       comm1='ncks -O  -v '+ param+' -d '+xvar +' '+str(jnum)+' -d '+ yvar +' '+str(inum)+' '+filein+' '+fileout
       os.system(comm1)
 
+from matplotlib.dates import date2num
+def stick_plot(time, u, v, **kw):
+    width = kw.pop('width', 0.002)
+    headwidth = kw.pop('headwidth', 0)
+    headlength = kw.pop('headlength', 0)
+    headaxislength = kw.pop('headaxislength', 0)
+    angles = kw.pop('angles', 'uv')
+    ax = kw.pop('ax', None)
+    
+    if angles != 'uv':
+        raise AssertionError("Stickplot angles must be 'uv' so that"
+                             "if *U*==*V* the angle of the arrow on"
+                             "the plot is 45 degrees CCW from the *x*-axis.")
+
+    time, u, v = map(np.asanyarray, (time, u, v))
+    if not ax:
+        fig, ax = plt.subplots()
+    
+    q = ax.quiver(date2num(time), [[0]*len(time)], u, v,
+                  angles='uv', width=width, headwidth=headwidth,
+                  headlength=headlength, headaxislength=headaxislength,
+                  **kw)
+
+    ax.axes.get_yaxis().set_visible(False)
+    ax.xaxis_date()
+    return q
 
 
