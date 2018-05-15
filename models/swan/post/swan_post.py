92c51c912cee5bf4b46e8b7187a15f461bae4c0f
Garcia Medina <gabriel.garciamedina@pnnl.gov>
2018-05-14 18:21:35 -0700

    table output to netCDF, lots of hard coded stuff.

--- models/swan/post/swan_post.py
+++ models/swan/post/swan_post.py
@@ -30,6 +30,12 @@ __email__ = "ggarcia@coas.oregonstate.edu"
 __group__ = 'Nearshore Modeling Group'
 
 # Import Modules
+import netCDF4 as _netCDF4
+import time as _time
+import sys as _sys
+import getpass as _getpass
+import pynmd.matlab as _gmat
+
 import numpy as np
 import datetime
 
@@ -210,5 +216,242 @@ def read_spec(specfile):
             'coords':coords,'info':info,'ot':ot}
     
 
+# ==============================================================================
+# Convert UnSWAN stations to netCDF4
+# ==============================================================================
+
+def unSWANCompToNetcdf(swanFile,swanGrid):
+    """
+
+    Convert UnSWAN results over all the computational grid to NetCDF4
+
+    PARAMETERS:
+    -----------
+    swanFile - Ascii file with bulk parameter output over all computational
+               grid
+    swanGrid - The fort.14 file (for now)
+    
+    RETURNS:
+    --------
+    NetCDF file with the same name as swanFile with .nc extension    
+
+    NOTES:
+    ------
+    1. Only works with fort.14 files
+    2. It is assumed that the first three variables in swanFile are:
+       Time Xp Yp ...
+
+    TODO:
+    -----
+    1. Implement conversion from stationary conditions
+    2. Implement ability to read from Triangle
+    
+    """
+    #import os
+    #swanFile = os.path.abspath(r"W:\sourceTerms\swan\WS-ns-201601\Stations_CG_buoy_201601.tab")
+    #swanGrid = os.path.abspath(r"W:\sourceTerms\swan\WS-ns-201601\fort.14")
+    
+    # Read grid file -----------------------------------------------------------
+
+    fobj = open(swanGrid)
+    fobj.readline()
+    tmpline = fobj.readline().split()
+    nele = np.int(tmpline[0])
+    npt = np.int(tmpline[1])
+
+    xp = np.zeros((npt,))
+    yp = np.zeros_like(xp)
+
+    # Read the grid (just in case)
+    for aa in range(npt):
+        tmpline = fobj.readline().split()
+        xp[aa] = np.float(tmpline[1])
+        yp[aa] = np.float(tmpline[2])
+
+    # Read triangles
+    triang = np.zeros((nele,3),dtype=np.int)
+    for aa in range(nele):
+        tmpline = fobj.readline().split()
+        triang[aa,0] = np.int(tmpline[2])
+        triang[aa,1] = np.int(tmpline[3])
+        triang[aa,2] = np.int(tmpline[4])
+
+    # All done here
+    fobj.close()
+
+    # Remember zero counting in python
+    triang -= 1
+
+    # Read the computational grid output ---------------------------------------
+    fobj = open(swanFile)
+
+    # Discard the first two lines
+    fobj.readline()
+    fobj.readline()
+
+    # Get the model information
+    modInfo = fobj.readline().split(':')
+    modInfo = {'Run':modInfo[1].split(' ')[0],
+            'Table':modInfo[2].split(' ')[0],
+            'SWAN':modInfo[3][:-1]}
+
+    # Empty line
+    fobj.readline()
+
+    # Variables
+    varkeys = fobj.readline()[1:].split()
+    # varkeys = fobj.readline()[1:-1]
+    # varkeys = [item for item in filter(None,varkeys.split(' '))]
+
+    # Find if the grid information is in the file
+    gridFlag = 'Xp' in varkeys and 'Yp' in varkeys
+
+    # Units
+    units = fobj.readline()[1:].split()[1:]
+    units[0] = '[UTC]' # Manually overwrite
+
+    # Empty line
+    fobj.readline()
+
+    # Find all nodes in file
+    tmpLine = fobj.readline().split()
+    date0 = tmpLine[0]
+    if gridFlag:
+        xp = []
+        yp = []
+        xp.append(np.float64(tmpLine[1]))
+        yp.append(np.float64(tmpLine[2]))
+    outpnt = 1
+    for line in fobj:
+        if line.split()[0] == date0:
+            if gridFlag:
+                xp.append(np.float64(line.split()[1]))
+                yp.append(np.float64(line.split()[2]))      
+            outpnt += 1
+        else:
+            break
+
+    if gridFlag:
+        xp = np.asarray(xp)
+        yp = np.asarray(yp)
+
+    # Close the file
+    fobj.close()
+
+    # Sanity check here
+    #if outpnt != npt:
+    #    print("Dimensions of grid and output are different")
+    #    return
 
 
+    # Read File and Prepare NetCDF File ----------------------------------------
+    print("Creating netCDF file")
+
+    # Create the file and add global attributes
+    outFile = swanFile + '.nc'
+    nc = _netCDF4.Dataset(outFile, 'w', format='NETCDF4')
+    nc.Description = 'UnSWAN Output'
+    nc.Author = _getpass.getuser()
+    nc.Created = _time.ctime()
+    nc.Software = 'Created with Python ' + _sys.version
+    nc.NetCDF_Lib = str(_netCDF4.getlibversion())
+    nc.Source = swanFile
+    nc.Run = modInfo['Run']
+    nc.Version = modInfo['SWAN']
+    nc.Table = modInfo['Table']
+
+    # Create dimensions
+    nc.createDimension('three',3)
+    nc.createDimension('nele',nele) # Number of elements
+    nc.createDimension('npnt',npt) # Number of nodes
+    nc.createDimension('time',0) # The unlimited dimension
+
+    # Create time vector
+    nc.createVariable('ocean_time','f8',('time'))
+    nc.variables['ocean_time'].units = 'seconds since 1900-01-01 00:00:00'
+    nc.variables['ocean_time'].calendar = 'julian'
+
+    nc.createVariable('matlab_time','f8',('time'))
+    nc.variables['matlab_time'].units = 'Days since 0000-01-01 00:00:00'
+
+    # Write coordinates
+    nc.createVariable(varkeys[1],'f8',('npnt'))
+    nc.variables[varkeys[1]].units = units[1][1:-1]
+    nc.variables[varkeys[1]][:] = xp
+
+    nc.createVariable(varkeys[2],'f8',('npnt'))
+    nc.variables[varkeys[2]].units = units[2][1:-1]
+    nc.variables[varkeys[2]][:] = yp
+
+    # Write the elements
+    nc.createVariable('elements','f8',('nele','three'))
+    nc.variables['elements'].long_name = 'Triangulation'
+    nc.variables['elements'][:] = triang
+
+    # Create the rest of the variables
+    for aa in range(3,len(varkeys)):
+        nc.createVariable(varkeys[aa],'f8',('time','npnt'))
+        nc.variables[varkeys[aa]].units = units[aa][1:-1]
+
+    # ==============================================================================
+    # Read and write variables to netCDF file
+    # ==============================================================================
+
+    print("  Reading and writing the file contents ...")
+
+    # First three variables are already considered
+    varkeys = varkeys[3:]
+
+    # Load the swan file again
+    fobj = open(swanFile)
+
+    # Ignore the first seven lines
+    for aa in range(7):
+        fobj.readline()
+
+    # Preallocate variable container
+    #   If the grid is not crazy large this should be ok
+    tmpvars = np.zeros((len(varkeys),npt))
+    cnt = 0
+    tstep = -1 # Time step counter (location in netcdf file)
+
+    # Loop until the end of the file
+    for line in fobj:
+        
+        # Increase counter variable
+        cnt += 1
+        
+        # Temporarily allocate variables
+        aa = np.asarray([np.float64(aa) for aa in line.split()])
+        tmpvars[:,cnt-1] = aa[3:]
+
+        # Write variables if all nodes have been read for the current time
+        if cnt == npt:
+
+            # Quick update
+            print('  Storing ' + line.split()[0])
+
+            # Increase time step variable
+            tstep += 1
+
+            # Time management
+            timePython = datetime.datetime.strptime(line.split()[0],
+                                                    "%Y%m%d.%H%M%S")
+            nc.variables['matlab_time'][tstep] = gmat.datetime_to_datenum(timePython)
+            ocean_time = timePython - datetime.datetime(1900,1,1)
+            nc.variables['ocean_time'][tstep] = ocean_time.total_seconds()
+
+            # Write variables to netcdf file
+            for aa in range(len(varkeys)):
+                nc.variables[varkeys[aa]][tstep,...] = tmpvars[aa,:]
+
+            # Reset counter variable
+            cnt = 0
+
+            # Reset variable container
+            tmpvars *= np.NAN
+
+
+    # All done here
+    fobj.close()
+    nc.close()
