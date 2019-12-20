05f694c2d058fd38b7c9b8dba31a3e3cb44e904d
ticonaf <49655627+ticonarf@users.noreply.github.com>
2019-12-20 14:10:38 -0800

    function to read adcirc max63 files

--- models/adcirc/post/adcirc_post.py
+++ models/adcirc/post/adcirc_post.py
@@ -29,14 +29,15 @@ import pynmd.models.adcirc.pre as adcpre
 # ==============================================================================
 # Read Fort 63 ASCII files and save as nc file
 # ==============================================================================
-def fort63_to_nc(fort63,varname='zeta',longname='water surface elevation above geoid',
-                varunits='m',**kwargs):
+def fort63_to_nc(fort63,varname='zeta',
+                 longname='water surface elevation above geoid',
+                 varunits='m',**kwargs):
     """ 
-    Script to read fort.63-type files and store in a netcdf4 file
+    Script to read fort.63-type (scalar) files and store in a netcdf4 file
 
     PARAMETERS:
     -----------
-    fort63: Path to fort63 file
+    fort63: Path to fort63-type file
 
     RETURNS:
     --------
@@ -105,11 +106,11 @@ def fort64_to_nc(fort64,varname_xy=['u-vel','v-vel'],
                  longname='water column vertically averaged',
                  varunits='m s-1',**kwargs):
     """ 
-    Script to read fort.64-type files and store in a netcdf4 file
+    Script to read fort.64-type (vector) files and store in a netcdf4 file
 
     PARAMETERS:
     -----------
-    fort64: Path to fort64 file
+    fort64: Path to fort64-type file
 
     RETURNS:
     --------
@@ -177,6 +178,86 @@ def fort64_to_nc(fort64,varname_xy=['u-vel','v-vel'],
     nc.close()
 
 
+# ==============================================================================
+# Read Global Maximum and Minimum ASCII files and save as nc file
+# ==============================================================================
+def max63_to_nc(max63,varname='zeta',
+                 longname='water surface elevation above geoid',
+                 varunits='m',**kwargs):
+    """ 
+    Script to read max63-type (max-min) files and store in a netcdf4 file
+
+    PARAMETERS:
+    -----------
+    max63: Path to max63-type file
+
+    RETURNS:
+    --------
+    Netcdf containing
+    time         : seconds since beginning of run 
+    x,y variable : temporal variables (name provided in 'xy_varname') recorded
+                   at the nodes of an unstructured grid. Size: [time,nodes]
+    """
+    
+    fobj = open(max63,'r')
+    
+    # Create the file and add global attributes
+    if 'savename' in kwargs:
+        ncfile = kwargs['savename']
+    else:
+        ncfile = max63 + '.nc'
+    nc = netCDF4.Dataset(ncfile, 'w', format='NETCDF4')
+    
+    # Global attributes 
+    nc.Author = getpass.getuser()
+    nc.Created = time.ctime()
+    tmpline = fobj.readline()
+    nc.description = tmpline[:32]
+    nc.rundes = tmpline[:32]
+    nc.runid = tmpline[32:56]
+    nc.model = 'ADCIRC'    
+    nc.Software = 'Created with Python ' + sys.version
+    nc.NetCDF_Lib = str(netCDF4.getlibversion())
+    
+    # Record number of time steps and nodes
+    nodes = np.int(fobj.readline().split()[1])
+    
+    # Create dimensions
+    nc.createDimension('node',nodes)       # Number of nodes
+    
+    # Create time vector
+    nc.createVariable('time','f8',('time'))
+    nc.variables['time'].long_name = 'model time'
+    nc.variables['time'].standard_name = 'time'
+    nc.variables['time'].units = 'seconds since beginning of run'
+    
+    # Create the rest of the variables
+    nc.createVariable(varname+'_max','f8','node')
+    nc.variables[varname+'_max'].long_name = 'maximum ' + longname
+    nc.variables[varname+'_max'].units = varunits
+    
+    nc.createVariable('time_of_'+varname+'_max','f8','node')
+    nc.variables['time_of_'+varname+'_max'].long_name = 'time of maximum ' + longname
+    nc.variables['time_of_'+varname+'_max'].units = 'sec'
+    
+    # Store last time-step in run for reference
+    nc.variables['time'][0] = np.float64(fobj.readline().split()[0])
+
+    # Store max variable observations 
+    for aa in range(nodes):
+        nc.variables[varname+'_max'][aa] = np.float64(fobj.readline().split()[1])
+    
+    # Skip repeated time 
+    fobj.readline()
+    
+    for aa in range(nodes):
+        nc.variables['time_of_'+varname+'_max'][aa] = np.float64(fobj.readline().split()[1])
+    
+    # All done here
+    fobj.close()
+    nc.close()
+    
+    
 # ==============================================================================
 # Read all ASCII files and save as nc file
 # ==============================================================================
@@ -254,3 +335,21 @@ def all_ascii2nc(runFld,ncFld):
             fort64_to_nc(runFld + 'fort.74',varname_xy=['windx','windy'],
                          longname='wind',savename=ncFld+'fort.74.nc')
     
+    # max.63-type files (max/min values) ---------------------------------------
+    # Water surface elevation
+    if os.path.exists(runFld+'maxele.63'):
+        if not os.path.exists(ncFld+'maxele.63.nc'):
+            print('Creating maxele.63.nc')
+            max63_to_nc(runFld + 'maxele.63',savename=ncFld+'maxele.63.nc')
+    if os.path.exists(runFld+'maxvel.63'):
+        if not os.path.exists(ncFld+'maxvel.63.nc'):
+            print('Creating maxvel.63.nc')
+            max63_to_nc(runFld + 'maxvel.63',varname='vel',
+                        longname='water velocity',
+                        varunits='m s-1',savename=ncFld+'maxvel.63.nc')
+    if os.path.exists(runFld+'maxwvel.63'):
+        if not os.path.exists(ncFld+'maxwvel.63.nc'):
+            print('Creating maxwvel.63.nc')
+            max63_to_nc(runFld + 'maxwvel.63',varname='wind',
+                        longname='wind velocity',
+                        varunits='m s-1',savename=ncFld+'maxwvel.63.nc')
