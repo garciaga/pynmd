69b9182002ff1469353caffc5fb1f2c4e9bba8f3
garciaga <gabrielgarciamedina@gmail.com>
2021-05-14 10:01:57 -0700

    Added x tick alignment function

--- plotting/general.py
+++ plotting/general.py
@@ -73,7 +73,71 @@ def align_yticks(ax1,ax2,color1=True,color2=True):
         ax2.set_ylabel(ax2.get_ylabel(),color=tmpHandle.get_color())
         
     return ax1,ax2
+
+
+def align_xticks(ax1,ax2,color1=True,color2=True):
+    """
+    Make ax2 have the x axis ticks at the same location as ax1
     
+    PARAMETERS:
+    -----------
+    ax1    : Handle of the primary axis
+    ax2    : Handle of the secondary axis (the one that will be manipulated)
+    color# : If true makes ytick and ylabel take the color of the last plotted
+             line.
+             
+    RETURNS:
+    --------
+    ax1,ax2
+    
+    TODO:
+    -----
+    align_xticks
+    
+    """
+    
+    # Get the data from the axes
+    # Primary axis
+    lims1  = ax1.get_xlim()
+    ticks1 = ax1.get_xticks()
+    
+    # Secondary axis
+    lims2  = ax2.get_xlim()
+    ticks2 = ax2.get_xticks()
+    
+    # Non-dimensionalize the primary ticks
+    oldTicks = (ticks1 - lims1[0])/(lims1[1] - lims1[0])
+    
+    # Set the new ticks
+    newTicks = oldTicks * (lims2[1] - lims2[0]) + lims2[0]
+    ax2.set_xticks(newTicks)
+    
+    # Set the limits again
+    # This is relevant if there are ticks outside of the plotting window
+    # I.E. newTicks < 0 or newTicks > 1
+    ax2.set_xlim(lims2)
+    
+    # The new grid is now redundant
+    ax2.grid('off')
+
+    # Color the ticks and label based on the last plot color
+    if color1:
+
+        # get the last color plotted
+        tmpHandle = ax1.get_lines()[-1]
+        ax1.tick_params('x',colors=tmpHandle.get_color())
+        ax1.set_xlabel(ax1.get_xlabel(),color=tmpHandle.get_color())
+    
+    # Color the ticks and label based on the last plot color
+    if color2:
+        
+        # get the last color plotted
+        tmpHandle = ax2.get_lines()[-1]
+        ax2.tick_params('x',colors=tmpHandle.get_color())
+        ax2.set_xlabel(ax2.get_xlabel(),color=tmpHandle.get_color())
+        
+    return ax1,ax2
+
 
 def stick_plot(time, u, v, **kw):
     """
